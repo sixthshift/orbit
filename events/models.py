2466ea3e76e2594d9008b52fdc6ba7b9aad52a82
Jason Huang <sixth.shifts@gmail.com>
2017-09-30 20:21:08 +1000

    More time UI additions

--- events/models.py
+++ events/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.timesince import timesince
 from uuslug import uuslug
 from accounts.models import Account
 from pages.models import Page
@@ -13,13 +14,18 @@ class Event(Page):
     # TODO ensure start is before end
 
     @property
+    def duration(self):
+        return timesince(d=self.start, now=self.end)
+
+    @property
     def display_date_range(self):
         if self.start.month == self.end.month and self.start.day == self.end.day:
             return self.start.strftime('%d %b')
         else:
             return self.start.strftime('%d %b') + ' - ' + self.end.strftime('%d %b')
 
-    def display_full_date_range(self):
+    @property
+    def display_date_time_range(self):
         return self.start.strftime('%Y, %b %d, %I:%M%p') + ' - ' + self.end.strftime('%Y, %b %d, %I:%M%p')
 
     def get_absolute_url(self):
